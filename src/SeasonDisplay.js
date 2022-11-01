27d9fb1916b71f6c43e9d5d555952b0143c9a5bc
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-11-01 19:09:39 +0100

    Update

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  return (
+    <div>
+      <h1>
+        {season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'}
+      </h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
